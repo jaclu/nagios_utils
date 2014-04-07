f6a0b63498f7ce7961bf8d498cb0c582292a0080
Jacob Lundqvist <jacob.lundqvist@gmail.com>
2014-04-07 11:37:30 +0200

    tab removal

--- check_http.py
+++ check_http.py
@@ -10,9 +10,9 @@ import sys
 
 def check_url(url,s_expect):
     try:
-    	f = urllib2.urlopen(url, timeout=10)
+        f = urllib2.urlopen(url, timeout=10)
     except:
-	f = urllib2.urlopen(url)
+        f = urllib2.urlopen(url)
     if f.code != 200:
         print 'ERROR: %s %s' % (f.code, f.msg)
         sys.exit(2)
@@ -21,12 +21,12 @@ def check_url(url,s_expect):
         print 'ERROR %s not found' % s_expect
         sys.exit(2)
     return
-        
-        
-        
+
+
+
 progname="./check_http.py "
 url=sys.argv[1]
 s_expect=sys.argv[2]
-    
+
 check_url(url,s_expect)
 print 'OK - HTTP/1.1 200 OK'
