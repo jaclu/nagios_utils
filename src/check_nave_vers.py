47ee082d6b2ef671e0323933ed8f8ac95b2441a3
jaclu <jacob.lundqvist@gmail.com>
2017-02-21 14:34:40 +0100

    updared features and some bugfixes

--- src/check_nave_vers.py
+++ src/check_nave_vers.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 """
  check_portal_rev.py for nagios
- Copyright 2014: europeana   License: EUPL
- Written by: Jacob.Lundqvist@europeana.eu
- jaclu 2014-04-07 vers 0.1.3
+ Copyright 2017:   License: GPL
+ Written by: Jacob.Lundqvist@gmail.com
+ jaclu 2017-02-21 vers 0.1.1
 
- Either just displays portal rev, or ensures specific version
 """
 
 
@@ -13,16 +12,18 @@ from naglib.nagiosplugin import NagiosPlugin
 
 
 class CheckNaveRev(NagiosPlugin):
-    VERSION = '0.1.0' # bad revision triggers warn instead of crit
-    DESCRIPTION = "Displays portal version"
+    VERSION = '0.1.1'
+    DESCRIPTION = "Displays nave version"
     CMD_LINE_HINT = 'host'
     ARGC = 1
 
     def custom_options(self, parser):
         parser.add_option("-r", "--rev", dest="nave_rev",
                           help="if specified specifies minimal accepted version")
+        parser.add_option("-l", "--lax", dest="relaxed", action='store_true', default=False,
+                          help="minor mismatch does not trigger WARN")
         parser.add_option("-p", "--precise", dest="precise", action='store_true', default=False,
-                          help="version must match exactly, otherwise minor rev can ")
+                          help="version must match exactly")
         parser.add_option("-u", "--url", dest="url", default="/version")
 
     def do_rev_check(self, nave_vers):
@@ -35,15 +36,27 @@ class CheckNaveRev(NagiosPlugin):
         #
         # Parsing params
         #
-        if len(self.options.nave_rev.split('.')) != 3:
-            self.exit_crit('nave_rev should be in x.y.z format')
-        p_maj = '.'.join(self.options.nave_rev.split('.')[:2])
-        p_min = self.options.nave_rev.split('.')[2]
+        # get rid of non numeric prefix
+        while self.options.nave_rev[0] not in '1234567890':
+            self.options.nave_rev = self.options.nave_rev[1:]
+        parts = self.options.nave_rev.split('.')
+        for p in parts:
+            try:
+                int(p)
+            except:
+                msg = 'Non integer part in version number: %s' % p
+                self.exit_crit(msg)
+        p_maj = '.'.join(parts[:2])
+        p_min = '.'.join(parts[2:])
 
         if p_maj != r_maj:
             self.exit_crit('Major vers missmatcch, expected:%s found:%s' % (self.options.nave_rev, nave_vers))
         if p_min > r_min:
-            self.exit_warn('Minor vers missmatcch, expected:%s found:%s' % (self.options.nave_rev, nave_vers))
+            msg = 'Minor vers missmatcch, expected:%s found:%s' % (self.options.nave_rev, nave_vers)
+            if self.options.relaxed:
+                self.exit_ok(msg)
+            else:
+                self.exit_warn(msg)
 
 
     def workload(self):
